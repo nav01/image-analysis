8833e2d4c386b6322ab3741329507ef081563841
Nav <nav01@mail.fresnostate.edu>
2018-08-30 18:07:14 -0700

    trial and error with heroku

--- backend/server.js
+++ backend/server.js
@@ -30,6 +30,6 @@ app.get('/profile/:id', (req, res) => profile.handleProfileGet(req, res, db));
 app.put('/image', (req, res) => image.handleImage(req,res, db));
 app.post('/imageurl', (req, res) => image.handleClarifaiCall(req, res));
 
-app.listen(process.env.PORT || 3001, () => {
+app.listen(process.env.PORT, () => {
   console.log('listening');
 });
